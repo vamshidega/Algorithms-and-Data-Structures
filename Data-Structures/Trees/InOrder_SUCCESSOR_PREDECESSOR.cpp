4d390f1923a92736c515c097ca6fc5a39d622b05
Vamsi Reddy <55816325+vamshidega@users.noreply.github.com>
2020-06-08 01:12:45 -0400

    Update InOrder_SUCCESSOR_PREDECESSOR.cpp

--- Data-Structures/Trees/InOrder_SUCCESSOR_PREDECESSOR.cpp
+++ Data-Structures/Trees/InOrder_SUCCESSOR_PREDECESSOR.cpp
@@ -1,7 +1,24 @@
+/**
+In Order successor and Predecessor
+
+Solution 1: Visit all the nodes in inorder traversal and store in an array and find the successor/Predecessor.
+Find the node in the array - immediate high value is the successor and immediate low value is the predecessor.
+
+Time complexity for Inorder traversal is O(n) - n is the number of nodes.
+-------------------------------------------------------------------------------
+Any query operation in a BST can be done in O(h) - h is the height of the tree
+-------------------------------------------------------------------------------
 
 
+Solution 2: Finding the Successor in O(h)
+CASE1: Given node has a right Subtree - then left most value or the minimum value in the right subtree is the Successor.
 
+CASE2: Given node has no right Subtree
+        The Nearest ancestor for which given node would be in left subtree is the successor
+        in other words - deepest ancestor node for which the left turn is taken is the successor.
 
+vice versa for predecessor node.
+**/
 
 
 
