32387bfd8ef8b12f4eed8cb493111d23a04b5c93
Vamsi Reddy <55816325+vamshidega@users.noreply.github.com>
2022-01-31 21:00:02 -0500

    Update Palindrome_problems.c

--- Algorithms/Palindrome_problems.c
+++ Algorithms/Palindrome_problems.c
@@ -1,1 +1,21 @@
+Palindrome Questions:
+
+1. Find the longest palindromic sub string in a string. (palindromic subsequence is different)
+2. Count all the palindromic sub strings in a series of strings / series of characters
+
+
+Find Longest Palindromic substring: Dynamic programming aproach.
+----------------------------------------------------------------
+  
+  
+  
+
+
+
+
+
+
+
+ 
+
 
